f016e03077c63f4ee40d70d832a2da23e046e19a
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-01-06 14:32:18 +0300

    First App. Constraints

---
+++ SB_First iOS Apps/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Mike on 05.01.2020.
+//  Copyright © 2020 Mike Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
