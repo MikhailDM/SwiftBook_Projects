60333c3181c27d16cc5275c7b3374d66123dd66e
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-06-23 11:55:38 +0300

    02_ToDoApp. Lesson 30

---
+++ SB_Testing/02_ToDoApp/ToDoAppTests/Controller/NewTaskViewControllerTests.swift
@@ -0,0 +1,113 @@
+//
+//  NewTaskViewControllerTests.swift
+//  ToDoAppTests
+//
+//  Created by Михаил Дмитриев on 23.06.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+import CoreLocation
+
+class NewTaskViewControllerTests: XCTestCase {
+
+    var sut: NewTaskViewController!
+    var placemark: MockCLPlacemark!
+    
+    override func setUp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: NewTaskViewController.self)) as? NewTaskViewController
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    //Наличие элементов
+    func testHasTitleTextField() {
+        XCTAssertTrue(sut.titleTextField.isDescendant(of: sut.view))
+    }
+    func testHasLocationTextField() {
+        XCTAssertTrue(sut.locationTextField.isDescendant(of: sut.view))
+    }
+    func testHasDateTextField() {
+        XCTAssertTrue(sut.dateTextField.isDescendant(of: sut.view))
+    }
+    func testHasAddressTextField() {
+        XCTAssertTrue(sut.addressTextField.isDescendant(of: sut.view))
+    }
+    func testHasDescriptionTextField() {
+        XCTAssertTrue(sut.descriptionTextField.isDescendant(of: sut.view))
+    }
+    func testHasCancelButton() {
+        XCTAssertTrue(sut.cancelButton.isDescendant(of: sut.view))
+    }
+    func testHasSaveButton() {
+        XCTAssertTrue(sut.saveButton.isDescendant(of: sut.view))
+    }
+    
+    //Сохранение геопозиции
+    func testSaveUsesGeocoderToConvertCoordinateFromAddress() {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        let date = df.date(from: "01.01.19")
+        
+        sut.titleTextField.text = "Foo"
+        sut.locationTextField.text = "Bar"
+        sut.dateTextField.text = "01.01.19"
+        sut.addressTextField.text = "Уфа"
+        sut.descriptionTextField.text = "Baz"
+        
+        sut.taskManager = TaskManager()
+        let mockGeocoder = MockCLGeocoder()
+        sut.geocoder = mockGeocoder
+        sut.save()
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 54.7373058, longitude: 55.9722491)
+        let location = Location(name: "Bar", coordinate: coordinate)
+        let generatedTask = Task(title: "Foo", description: "Baz", date: date, location: location)
+        
+        placemark = MockCLPlacemark()
+        placemark.mockCoordinate = coordinate
+        mockGeocoder.completionHandler?([placemark], nil)
+        
+        let task = sut.taskManager.task(at: 0)
+        
+        XCTAssertEqual(task, generatedTask)
+    }
+    
+    //Содержание метода Save
+    func testSaveButtonHasSaveMethod() {
+        let saveButton = sut.saveButton
+        
+        guard let actions = saveButton?.actions(forTarget: sut, forControlEvent: .touchUpInside) else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertTrue(actions.contains("save"))
+    }
+}
+
+//Фейковый класс
+extension NewTaskViewControllerTests {
+    class MockCLGeocoder: CLGeocoder {
+        
+        var completionHandler: CLGeocodeCompletionHandler?
+        
+        override func geocodeAddressString(_ addressString: String, completionHandler: @escaping CLGeocodeCompletionHandler) {
+            self.completionHandler = completionHandler
+        }
+    }
+    
+    class MockCLPlacemark: CLPlacemark {
+        
+        var mockCoordinate: CLLocationCoordinate2D!
+        
+        override var location: CLLocation? {
+            return CLLocation(latitude: mockCoordinate.latitude, longitude: mockCoordinate.longitude)
+        }
+    }
+}
